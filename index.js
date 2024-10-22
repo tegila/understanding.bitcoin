accebf246e2023fb64c07d06f2c583004799e101
Edson Tégila <105643+tegila@users.noreply.github.com>
2024-10-22 09:56:00 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const log = console.log;
 const { createHash } = require("crypto");
 
+log(process.argv);
+const second_step = {};
+[, , second_step.script_name] = process.argv;
+if (second_step.script_name)
+  second_step.callback = require(second_step.script_name);
+log(second_step);
+
 const _bitcoin_message_parser = (d) => {
   const command = d
     .slice(4, 12 + 4)
@@ -20,6 +27,8 @@ const _bitcoin_message_parser = (d) => {
   if (hash_1 != checksum) return hash_1 === checksum;
   log(command, checksum, hash_1, hash_1 === checksum);
   //log("rest", d.slice(12 + 4 + 4 + 4 + size));
+  if (second_step.script_name)
+    second_step.callback(payload, command, checksum, size);
 };
 
 const has_magic_bytes = /f9beb4d9/i;
@@ -33,7 +42,7 @@ process.stdin.on("readable", (c) => {
     if (!has_magic_bytes.test(chunk_string)) return;
     const m = chunk_string.match(has_magic_bytes);
     if (!m) return;
-    log(m.index / 2, chunk_string.length / 2, chunk.length);
+    //log(m.index / 2, chunk_string.length / 2, chunk.length);
     _bitcoin_message_parser(chunk.slice(m.index / 2));
   }
 });
